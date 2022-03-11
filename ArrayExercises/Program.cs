c53f55e2511cf68a714b7dab0f8292f1a174b828
Allison Anguiano <aanguiano@dev-10.com>
2022-03-10 21:37:27 -0600

    Get last element in array

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -5,19 +5,23 @@ namespace ArrayExercises
     class Program
     {
         static void Main()
-
-            /// Given a size greater than zero, return a new array of ints
-        //[TestCase(3, new int[3] { 0, 0, 0 })]
-        //[TestCase(1, new int[1] { 0 })]
-        //[TestCase(5, new int[5] { 0, 0, 0, 0, 0 })]
         {
-            int length = PromptUser4Int("Enter length of array: ");
-            int[] newArray = new int[length];
-            foreach (var entry in newArray)
+            ///// Given an array larger than 0, return the last element in the array
+            ///// </summary>
+            //[TestCase(new int[] { 1, 2, 3 }, 3)]
+            //[TestCase(new int[] { 1, 3, 7, 11, 5, 5 }, 5)]
+            //[TestCase(new int[] { 10 }, 10)]
+            string input = PromptUser("Give me an array of numbers larger than zero: ");
+            //string items = input.Replace(", ", "");
+            string[] Arr = input.Split(",");                //works if user enters as 1,2,3
+                                                            //may need to consider spaces and otherwise...
+                                                            //or have user enter one number at a time??
+
+            if (Arr.Length > 0)
             {
-                Console.Write(entry);
+                string lastItem = Arr[Arr.Length - 1];
+                Console.Write($"The last item is {lastItem}");
             }
-            //Console.Write($"{newArray}");
         }
 
         private static string PromptUser(string message)
