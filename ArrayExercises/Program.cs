05c3bcfe50d639fd4e3616a569c00afbea3a71f4
Allison Anguiano <aanguiano@dev-10.com>
2022-03-10 22:41:04 -0600

    Add more slots to an array, resize array

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+//using System.Linq;
 
 namespace ArrayExercises
 {
@@ -6,27 +7,35 @@ namespace ArrayExercises
     {
         static void Main()
         {
-            ///// Given an empty array, fill each element with the index + 1
+            ///// <summary>
+            ///// Given an array, and n. Add more slots to an array. eg. An array with 5 elements, and n = 2, return an array with 7 elements.
             ///// </summary>
-            //[TestCase(new int[3] { 0, 0, 0 }, new int[3] { 1, 2, 3 })]
-            //[TestCase(new int[1] { 0 }, new int[1] { 1 })]
-            //[TestCase(new int[5] { 0, 0, 0, 0, 0 }, new int[5] { 1, 2, 3, 4, 5 })]
+            //[TestCase(new int[3] { 1, 2, 3 }, 2, new int[5] { 1, 2, 3, 0, 0 })]
+            //[TestCase(new int[1] { 1 }, 3, new int[4] { 1, 0, 0, 0 })]
+            //[TestCase(new int[5] { 1, 2, 3, 4, 5 }, 1, new int[6] { 1, 2, 3, 4, 5, 0 })]
 
-            //Oops!!! I did where is was an empty string array, as in { , , } null values
-            //Would've been easier with numbers anyways
 
+            //I still have not been able to handle any other format besides 1,4,5 etc
             int i;
-            string input = PromptUser("Give me an empty array of any length: ");
+            string input = PromptUser("Give me a numeric array: ");
             string items = input.Replace("{ ", "");
             items = items.Replace("}", "");
-
             string[] Arr = items.Split(",");
+
             int[] intArr = new int[Arr.Length];
 
-            for (i = 0; i < Arr.Length; i++)
+            for (i = 0; i < intArr.Length; i++)
             {
-                intArr[i] = i+1;
+                intArr[i] = int.Parse(Arr[i]);
             }
+
+            int n = PromptUser4Int("Enter number of slots you wish to add: ");
+            for (i = 0; i < n; i++)
+            {
+                Array.Resize(ref intArr, intArr.Length + 1);
+                intArr[intArr.Length - 1] = 0;
+            }
+            
             foreach (var entry in intArr)
             {
                 Console.Write(entry);
@@ -36,6 +45,17 @@ namespace ArrayExercises
             {
                 Console.Write(message);
                 return Console.ReadLine();
+            }
+
+            static int PromptUser4Int(string message)
+            {
+                int result;
+                while (!int.TryParse(PromptUser(message), out result))
+                {
+                    PromptUser("Invalid Input! Press any key to continue");
+                }
+
+                return result;
             }
         }
 
